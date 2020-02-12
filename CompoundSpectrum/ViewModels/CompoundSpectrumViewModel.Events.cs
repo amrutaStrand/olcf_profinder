f837e6cf945df8eb64da0c18017c513bf0e628e6
shyamk <shyamk@strandls.com>
2020-02-12 17:23:39 +0530

    UpdatePlots code added... on event...

--- CompoundSpectrum/ViewModels/CompoundSpectrumViewModel.Events.cs
+++ CompoundSpectrum/ViewModels/CompoundSpectrumViewModel.Events.cs
@@ -1,7 +1,16 @@
 ﻿namespace Agilent.OpenLab.CompoundSpectrum
 {
+
     using DataTypes;
     using Events;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+
+
+    using Agilent.OpenLab.Framework.DataAccess.CoreTypes;
+    using Agilent.OpenLab.UI.Controls.GraphObjects;
+    using Agilent.OpenLab.UI.Controls.AgtPlotControl.Basic;
 
     /// <summary>
     /// CompoundSpectrumViewModel
@@ -14,7 +23,22 @@
         /// parameters used to track unhandled events
         /// </summary>
         private bool unhandledSomethingEventMonitored;
-
+        Color[] colorArray = new Color[] {
+                                            Color.Magenta,
+                                            Color.Red,
+                                            Color.Blue,
+                                            Color.Green,
+                                            Color.Cyan,
+                                            Color.BlueViolet,
+                                            Color.Black,
+                                            Color.Chocolate,
+                                            Color.Coral,
+                                            Color.DarkSlateBlue,
+                                            Color.AliceBlue,
+                                            Color.Aqua,
+                                            Color.Goldenrod,
+                                            Color.Honeydew
+                                            };
         #endregion
 
         #region Methods
@@ -47,8 +71,29 @@
 
         }
 
-        private void CompoundSelectionChanged(ICompoundGroup obj)
+        private void CompoundSelectionChanged(ICompoundGroup compoundGroupObject)
         {
+            this.PlotControl.RemoveAllItems();
+            if (compoundGroupObject == null) return;
+
+            IDictionary<string, ICompound> sampleWiseCompounds = compoundGroupObject.SampleWiseDataDictionary;
+            List<MsSpectrumGraphObject> msSpectrumGraphObjects = new List<MsSpectrumGraphObject>();
+
+            IEnumerator<ICompound> ce = sampleWiseCompounds.Values.GetEnumerator ();
+            int colorCounter = 0;
+            int colorArraySize = colorArray.Length;
+            while (ce.MoveNext())
+            {
+
+                Color color = colorArray[colorCounter];
+                colorCounter = (colorCounter + 1) % colorArraySize;
+                MsSpectrumGraphObject mso = GetMsSpectrumGraphObject(ce.Current, color);
+
+                msSpectrumGraphObjects.Add(mso);
+            }
+            updatePlotControl(msSpectrumGraphObjects);
+
+
         }
 
         /// <summary>
@@ -64,6 +109,109 @@
 
         }
 
+
+        private void updatePlotControl(List<MsSpectrumGraphObject> msSpectrumGraphObjects)
+        {
+            this.PlotControl.RemoveAllItems();
+            if (msSpectrumGraphObjects.Count <= 0) return;
+
+            int numberOfRows = msSpectrumGraphObjects.Count;
+            int numberOfColumns = 1;
+
+            int numberOfRowsToShow = numberOfRows;
+            if (numberOfRows > 3)
+                numberOfRowsToShow = 3;
+
+            this.PlotControl.Initialize(numberOfRows, numberOfColumns, numberOfRowsToShow, numberOfColumns);
+            this.PlotControl.GraphManager.ShowLegend = true;
+            this.PlotControl.GraphManager.ShowSignalHints = true;
+            this.PlotControl.ControlSettings.PaneSelectionMode = PaneSelectionMode.None;
+            foreach (var paneManager in this.plotControl.GraphManager.PaneManagers())
+            {
+                this.InitializePaneProperties(paneManager);
+            }
+
+            IEnumerator<MsSpectrumGraphObject> enumerator = msSpectrumGraphObjects.GetEnumerator();
+            int horizontalCounter = 0;
+            while (enumerator.MoveNext())
+            {
+                this.PlotControl.AddItem(horizontalCounter++, 0, enumerator.Current);
+
+            }
+
+        }
+
+
+        /// <summary>
+        /// The get mass of most abundant ion.
+        /// </summary>
+        /// <param name="spectrumData">
+        /// The spectrum data.
+        /// </param>       
+        public static double? GetMassOfMostAbundantIon(ISpectrumData spectrumData)
+        {
+            var maxY = spectrumData.Data.YValues.Max();
+            for (var i = 0; i < spectrumData.Data.YValues.Length; ++i)
+            {
+                if (spectrumData.Data.YValues[i] == maxY)
+                {
+                    return spectrumData.Data.XValues[i];
+                }
+            }
+
+            return null;
+        }
+
+
+        static MsSpectrumGraphObject GetMsSpectrumGraphObject(ICompound compound, Color color)
+        {
+            string spectrumName = compound.FileName;
+            SpectrumData spectrumData = getSpectrumData(compound);
+            var spectrumGraphObject = new MsSpectrumGraphObject(spectrumData);
+
+            spectrumGraphObject.DisplaySettings.Color = color;
+            spectrumGraphObject.CreateLegendObject(
+                new List<string>
+                    {
+                        string.Format("{0};  #Datapoints = {1}", spectrumName, spectrumData.Data.XValues.Length),
+                        string.Format("Max. Abundance: {0:0}", spectrumData.Data.YValues.Max())
+                    });
+            spectrumGraphObject.MassOfPrecursorIon = GetMassOfMostAbundantIon(spectrumData);
+            spectrumGraphObject.DisplaySettings.ShowPrecursorIonAnnotation = true;
+            spectrumGraphObject.DisplaySettings.PenSizeFocusedIon = 2f;
+            spectrumGraphObject.DisplaySettings.PenSizeDefaultIon = 1f;
+
+            // spectrumGraphObject.DisplaySettings.DefaultIonAnnotationFont = GraphicToolsRepository.FontArial10;
+            // spectrumGraphObject.DisplaySettings.FocusedIonAnnotationFont = GraphicToolsRepository.FontArial12Bold;
+            spectrumGraphObject.DisplaySettings.IonAnnotationFormatString = "{0:0.000}";
+            spectrumGraphObject.DisplaySettings.IonAnnotationLabelingMode = IonAnnotationLabelingMode.Always;
+            spectrumGraphObject.DisplaySettings.IonAnnotationLabelsMayOverlapIons = true;
+            spectrumGraphObject.DisplaySettings.MaximumNumberOfVisibleIonAnnotationLabels = 20;
+            return spectrumGraphObject;
+
+        }
+
+        static SpectrumData getSpectrumData(ICompound compound)
+        {
+            ICollection<IPeak> peaks = compound.Spectrum.Peaks;
+            int count = peaks.Count;
+
+            double[] xValues = new double[count];
+            double[] yValues = new double[count];
+
+            IEnumerator<IPeak> pe = peaks.GetEnumerator();
+            int counter = 0;
+            while (pe.MoveNext())
+            {
+                IPeak peak = pe.Current;
+                xValues[counter] = peak.X;
+                yValues[counter] = peak.Y;
+                counter++;
+            }
+
+            return new SpectrumData(new Data(xValues, yValues), XUnit.MassToCharge, "Counts", 13.56);
+
+        }
         #endregion
     }
 }
