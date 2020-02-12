ef806926e2717c0f0d4fd3aedaea86d1cc8024cb
shyamk <shyamk@strandls.com>
2020-02-12 15:08:10 +0530

    EVents added...

---
+++ Events/CompoundGroupsGenerated.cs
@@ -0,0 +1,13 @@
+﻿namespace Events
+{
+    using Microsoft.Practices.Prism.Events;
+    using DataTypes;
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///     The loaded songs changed event.
+    /// </summary>
+    public class CompoundGroupsGenerated : CompositePresentationEvent<List<ICompoundGroup>>
+    {
+    }
+}
---
+++ Events/CompoundSelectionChanged.cs
@@ -0,0 +1,14 @@
+﻿namespace Events
+{
+    using Microsoft.Practices.Prism.Events;
+    using DataTypes;
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///     The loaded songs changed event.
+    /// </summary>
+    public class CompoundSelectionChanged : CompositePresentationEvent<ICompoundGroup>
+    {
+
+    }
+}
