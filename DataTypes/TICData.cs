ca65636586466eecd09a66a739fb2639216dc2a3
Shivam Bhosale <shivam.bhosale@strandls.com>
2020-02-19 16:20:17 +0530

    cleaned code in TICData.cs

--- DataTypes/TICData.cs
+++ DataTypes/TICData.cs
@@ -1,5 +1,4 @@
-﻿using Agilent.MassSpectrometry.DataAnalysis;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,83 +14,12 @@ namespace DataTypes
 
         public float[] YArray => m_yArray;
 
-        public TICData(string FileName)
+        public TICData(double[] xArray, float[] yArray)
         {
-            try
-            {
-                if (FileName != null)
-                {
-                    DOReadTICTest1(FileName);
-                }
-                else
-                {
-                    throw new Exception("File name is null. So cant produce TIC data.");
-                }
-
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            m_xArray = xArray;
+            m_yArray = yArray;
         }
 
-        private void DOReadTICTest1(string FileName)  // data file name, such as c:\temp\mydata.d
-
-        {
-
-            IDataAccess dataAccessor = new DataAccess() as IDataAccess;
-
-            string sFileName = string.Empty;
-
-            try
-
-            {
-
-                double[] m_allRtArray = null;       // RT  x-value
-
-                float[] m_allTicYArray = null;      // TIC y-values
-
-                sFileName = FileName;
-
-                dataAccessor.OpenDataFile(sFileName);
-
-                var m_BDADataAccess = dataAccessor.BaseDataAccess;
-
-                IBDAChromFilter psetfilter = new BDAChromFilter();
-
-                psetfilter.ChromatogramType = ChromType.TotalIon;
-
-                IBDAChromData[] chromarray = m_BDADataAccess.GetChromatogram(psetfilter);
-
-                m_allRtArray = chromarray[0].XArray;
-
-                m_allTicYArray = chromarray[0].YArray;
-
-                //setting values in data members
-                m_xArray = m_allRtArray;
-                m_yArray = m_allTicYArray;
-
-            }
-
-            catch (Exception ex)
-
-            {
-
-                // MessageBox.Show(ex.Message);
-                throw;
-
-            }
-
-            finally
-
-            {
-
-                if (!string.IsNullOrEmpty(sFileName))
-
-                    dataAccessor.CloseDataFile();  // make sure the data file will be closed
-
-            }
-
-        }
+        
     }
 }
