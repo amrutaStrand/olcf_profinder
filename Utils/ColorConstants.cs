60756e1a91a14651452f46dfe68ab0efb84dc175
shyamk <shyamk@strandls.com>
2020-03-02 10:50:30 +0530

    Uitls added to handle the color constant.s...

---
+++ Utils/ColorConstants.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace Utils
+{
+    public class ColorConstants
+    {
+        public static Color[] COLOR_ARRAY = new Color[] {
+                                            Color.Magenta,
+                                            Color.Red,
+                                            Color.Blue,
+                                            Color.Green,
+                                            Color.Cyan,
+                                            Color.BlueViolet,
+                                            Color.Black,
+                                            Color.Chocolate,
+                                            Color.Coral,
+                                            Color.DarkSlateBlue,
+                                            Color.AliceBlue,
+                                            Color.Aqua,
+                                            Color.Goldenrod,
+                                            Color.Honeydew
+                                            };
+    }
+}
