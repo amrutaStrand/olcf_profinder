72d4f27a93d2704e49806598471f0adf5e6fd66e
harika <harika@strandls.com>
2020-03-05 15:32:55 +0530

    Adding feature extractor ui module

---
+++ FeatureExtractionUI/FeatureExtractionUIModule.IModuleInfo.cs
@@ -0,0 +1,65 @@
+﻿namespace Agilent.OpenLab.FeatureExtractionUI
+{
+    #region
+
+    using System;
+    using System.Windows.Media.Imaging;
+
+    using Agilent.OpenLab.FeatureExtractionUI.Properties;
+
+    #endregion
+
+    partial class FeatureExtractionUIModule
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the caption.
+        /// </summary>
+        public override string Caption
+        {
+            get
+            {
+                return Resources.FeatureExtractionUICaption;
+            }
+        }
+
+        /// <summary>
+        /// Gets the image.
+        /// </summary>
+        public override BitmapImage Image
+        {
+            get
+            {
+                return
+                    new BitmapImage(
+                        new Uri(
+                            "pack://application:,,,/Agilent.OpenLab.FeatureExtractionUI;component/Images/TestImage.png"));
+            }
+        }
+
+        /// <summary>
+        /// Gets the key tip.
+        /// </summary>
+        public override string KeyTip
+        {
+            get
+            {
+                return "M";
+            }
+        }
+
+        /// <summary>
+        /// Gets the tooltip.
+        /// </summary>
+        public override string Tooltip
+        {
+            get
+            {
+                return Resources.FeatureExtractionUIHint;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ FeatureExtractionUI/ViewModels/FeatureExtractionUIViewModel.cs
@@ -0,0 +1,53 @@
+﻿namespace Agilent.OpenLab.FeatureExtractionUI
+{
+    #region
+
+    using Agilent.OpenLab.Framework.UI.Module;
+
+    using Microsoft.Practices.Unity;
+
+    #endregion
+
+    /// <summary>
+    /// FeatureExtractionUIViewModel
+    /// </summary>
+    /// <remarks>
+    /// </remarks>
+    public partial class FeatureExtractionUIViewModel : BaseViewModel, IFeatureExtractionUIViewModel
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeatureExtractionUIViewModel"/> class.
+        /// </summary>
+        /// <param name="container">
+        /// The container. 
+        /// </param>
+        /// <remarks>
+        /// </remarks>
+        public FeatureExtractionUIViewModel(IUnityContainer container)
+            : base(container)
+        {
+            this.View = this.UnityContainer.Resolve<IFeatureExtractionUIView>();
+            this.View.Model = this;
+            this.SubscribeEvents();
+            this.InitializeCommands();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the view.
+        /// </summary>
+        /// <value>
+        /// The view. 
+        /// </value>
+        /// <remarks>
+        /// </remarks>
+        public IFeatureExtractionUIView View { get; set; }
+
+        #endregion
+    }
+}
