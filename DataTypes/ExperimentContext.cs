86a06b927dcbd49e10e17c791b3fd4eb5bd1c10b
shyamk <shyamk@strandls.com>
2020-03-13 14:37:58 +0530

    Experiment Contect added...

---
+++ DataTypes/ExperimentContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ExperimentContext
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        List<ISample> Samples { set; get; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        List<ICompoundGroup> CompoundGroups { get; set; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Dictionary<string, string> GetGrouping()
+        {
+            return null;
+        }
+
+    }
+}
